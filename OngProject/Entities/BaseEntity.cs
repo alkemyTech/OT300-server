a4bd01464ada695f2955a1e67e20a5a2fb2a3c0a
Geraldson Perez <31606909+Geraldsonp@users.noreply.github.com>
2022-09-15 19:38:44 -0400

    adds base entity

---
+++ OngProject/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class BaseEntity
+    {
+        [Required]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime LastEditedAt { get; set; }
+
+    }
+}
