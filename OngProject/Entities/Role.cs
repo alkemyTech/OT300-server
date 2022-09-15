51594e6d720f7223d2f65f823a910811e8400221
DnTo <danto@hotmail.com>
2022-09-15 18:25:02 -0500

    Create Role mode classl, also empty User class

---
+++ OngProject/Entities/Role.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Role:BaseEntity
+    {
+        public Role()
+        {
+            Roles = new List<Role>();
+        }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
---
+++ OngProject/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    /// <summary>
+    /// User from the System
+    /// </summary>
+    public class User : BaseEntity
+    {
+      
+    }
+}
+
+
+
+
