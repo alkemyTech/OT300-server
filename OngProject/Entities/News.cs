90892234b7fd3f413fed6287ae279a4118e14cda
Matias Fernandez <matias_fernandez@live.com.ar>
2022-09-15 18:57:34 +0200

    Crear modelo y migracion base de News

---
+++ OngProject/Entities/News.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class News : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        public int IdCategory { get; set; }
+
+    }
+}
