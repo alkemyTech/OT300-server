a2cc4d9a2aa62acc82e3d95b166535ad1b8ceae8
DnTo <danto@hotmail.com>
2022-09-16 16:38:59 -0500

    add IUserInterface for further oparations

--- OngProject/Repositories/UserRepository.cs
+++ OngProject/Repositories/UserRepository.cs
@@ -1,16 +1,16 @@
-﻿using global::OngProject.Entities;
+﻿using OngProject.Entities;
 using Microsoft.EntityFrameworkCore;
 using OngProject.DataAccess;
-using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
 using System.Data.Common;
 using System.Runtime.CompilerServices;
 
 namespace OngProject.Repositories
 {
-    public class UserRepository : RepositoryBase<User>
+    public class UserRepository : RepositoryBase<User>, IUserRepository
     {
         private readonly OngDbContext context;
-        public UserRepository(OngDbContext context) :base(context)
+        public UserRepository(OngDbContext context) : base(context)
         {
             this.context = context;
         }
