1f4dacc2ed74117df3be7983728520ddf6b704e1
Matias <mgabrielpalacio@gmail.com>
2022-09-15 23:59:54 -0300

    Create MembersDTO and set the Nullable Properties

---
+++ OngProject/Core/Models/DTOs/MembersDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class MembersDTO
+    {
+        [Required(ErrorMessage ="Name is required"),MaxLength(50)]
+        public string Name { get; set; }
+        public string FacebookUrl { get; set; }
+        public string InstagramUrl { get; set; }
+        public string LinkedInUrl { get; set; }
+        [Required(ErrorMessage = "An image is required")]
+        public string Image { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
--- OngProject/Entities/Members.cs
+++ OngProject/Entities/Members.cs
@@ -11,20 +11,20 @@ namespace OngProject.Entities
         public string Name { get; set; }
 
         [DataType(DataType.Url)]
-        public string FacebookUrl { get; set; }
+        public string? FacebookUrl { get; set; }
 
         [DataType(DataType.Url)]
-        public string InstagramUrl { get; set; }
+        public string? InstagramUrl { get; set; }
 
         [DataType(DataType.Url)]
-        public string LinkedInUrl { get; set; }
+        public string? LinkedInUrl { get; set; }
 
         [Required(ErrorMessage = "An image is required")]
         public string Image { get; set; }
 
         //Even when description isn't required it's assign a max length to be really specific
         [MaxLength(255)]
-        public string Description { get; set; }
+        public string? Description { get; set; }
        
     }
 }
