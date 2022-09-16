0d099ac5a24db10f2b82627fb0ca70725d6fe1a7
Geraldson Perez <31606909+Geraldsonp@users.noreply.github.com>
2022-09-15 20:03:41 -0400

    Update BaseEntity.cs

--- OngProject/Entities/BaseEntity.cs
+++ OngProject/Entities/BaseEntity.cs
@@ -6,6 +6,7 @@ namespace OngProject.Entities
     public class BaseEntity
     {
         [Required]
+        [Key]
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsDeleted { get; set; }
