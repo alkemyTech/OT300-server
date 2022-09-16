eb981ab8cd7632f3752e4de080003ba1edab71ed
DnTo <danto@hotmail.com>
2022-09-15 19:20:36 -0500

    Add User Model

--- OngProject/Entities/User.cs
+++ OngProject/Entities/User.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OngProject.Entities
 {
@@ -8,10 +8,25 @@ namespace OngProject.Entities
     /// </summary>
     public class User : BaseEntity
     {
-      
+        //PK from BaseEntity
+        [Required]  
+        public string FirstName { get; set; }
+        
+        [Required]     
+        public string LastName { get; set; }
+
+        [Required]  
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Photo { get; set; }
+
+        public Role  Role { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
     }
 }
-
-
-
-
