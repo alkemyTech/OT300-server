82ec090b565cc74761079683abb227abc9cb1f0d
DnTo <danto@hotmail.com>
2022-09-16 16:34:31 -0500

    add UserRepository

---
+++ OngProject/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using global::OngProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using OngProject.DataAccess;
+using OngProject.Entities;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+
+namespace OngProject.Repositories
+{
+    public class UserRepository : RepositoryBase<User>
+    {
+        private readonly OngDbContext context;
+        public UserRepository(OngDbContext context) :base(context)
+        {
+            this.context = context;
+        }
+
+
+    }
+}
+
