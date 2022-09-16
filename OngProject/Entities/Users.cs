2404e886f02f78d4e6ee6ac5b273d8b124624033
DnTo <danto@hotmail.com>
2022-09-16 12:09:53 -0500

    Create Users Model

---
+++ OngProject/Entities/Users.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace OngProject.Entities
+{
+    public class User : BaseEntity
+    {
+        //PK from BaseEntity
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Photo { get; set; }
+
+        public Role Role { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+    }
+}
