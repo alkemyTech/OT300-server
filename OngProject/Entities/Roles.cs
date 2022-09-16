af00a3fce9d5d833e8e98c4fcd31cb9a7c230a15
DnTo <danto@hotmail.com>
2022-09-16 12:06:40 -0500

    Add Roles Model

---
+++ OngProject/Entities/Roles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Role : BaseEntity
+    {
+        public Role()
+        {
+            User = new List<Users>();
+        }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Users> Users { get; set; }
+    }
+
+}
