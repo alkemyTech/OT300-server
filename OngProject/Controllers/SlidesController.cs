196c59dc734a0b169e304f0b7b5366446e1dd8fa
adrianparedes477 <100713764+adrianparedes477@users.noreply.github.com>
2022-09-16 23:52:20 -0300

    modelo y controlador de Slides

---
+++ OngProject/Controllers/SlidesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OngProject.Controllers
+{
+    public class SlidesController : Controller
+    {
+        // GET: SlidesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SlidesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SlidesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SlidesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SlidesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SlidesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SlidesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SlidesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OngProject/Entities/Slides.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Slides : BaseEntity
+    {
+        [Required]
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+
+        public string Text { get; set; }
+
+        [Required]
+        public int Order { get; set; }
+
+        [Required]
+        public int OrganizationId { get; set; }
+    }
+}
