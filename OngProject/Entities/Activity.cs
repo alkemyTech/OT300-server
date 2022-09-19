9b187670d806ed65ad1a3f741554653d95a484f1
DnTo <danto@hotmail.com>
2022-09-19 02:06:53 -0500

    Create Activity Entity

---
+++ OngProject/Entities/Activity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OngProject.Entities
+{
+    public class Activity : BaseEntity
+    {
+        [Required]
+        [Column(TypeName = "varchar()")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(max)")]
+        public string Content { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar()")]
+        public string Image { get; set; }
+    }
+}
