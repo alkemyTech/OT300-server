a8babe1ea504671de924c123b0bff345e8536cb2
DnTo <danto@hotmail.com>
2022-09-19 21:53:18 -0500

    Add Testimonial Feature : Controller + Business + IBusiness

---
+++ OngProject/Core/Business/TestimonialBusiness.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+
+namespace OngProject.Core.Business
+{
+    public class TestimonialBusiness : ITestimonialBusiness
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TestimonialBusiness(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public User Insert(Testimonial request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Testimonial> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Testimonial> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Testimonial> Update(Testimonial user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Testimonial user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Testimonial> Add(Testimonial entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ OngProject/Core/Interfaces/ITestimonialBusiness.cs
@@ -0,0 +1,9 @@
+﻿using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface ITestimonialBusiness : IRepositoryBase<Testimonial>
+    {
+    }
+}
