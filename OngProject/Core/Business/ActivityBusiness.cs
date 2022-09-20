397e2296a6276ae7092b6ce3ef5087d17a0ddf25
DnTo <danto@hotmail.com>
2022-09-19 22:02:31 -0500

    Add Activity feature : Controller + Business

---
+++ OngProject/Core/Business/ActivityBusiness.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+
+namespace OngProject.Core.Business
+{
+    public class ActivityBusiness : IActivityBusiness
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ActivityBusiness(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Activity Insert(Activity request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Activity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Activity> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Activity> Update(Activity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Activity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Activity> Add(Activity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
