39b622858c1b8fb34dd520ce307facf6c7d12b67
adrianparedes477 <100713764+adrianparedes477@users.noreply.github.com>
2022-09-26 13:12:55 -0300

    History 73

--- OngProject/Core/Models/DTOs/OrganizationPublicDTO.cs
+++ OngProject/Core/Models/DTOs/OrganizationPublicDTO.cs
@@ -8,5 +8,8 @@ namespace OngProject.Core.Models.DTOs
         public string Img { get; set; }
         public string Adress { get; set; }
         public int PhoneNumber { get; set; }
+        public string FacebookUrl { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string InstagramUrl { get; set; }
     }
 }
