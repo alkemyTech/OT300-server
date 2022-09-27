296241239ec170860df22a4672c8c8907166a2c1
Matias <mgabrielpalacio@gmail.com>
2022-09-27 12:57:56 -0300

    Organization Seed

---
+++ OngProject/DataAccess/Seeds/OrganizationSeed.cs
@@ -0,0 +1,34 @@
+﻿using OngProject.Entities;
+using System;
+
+namespace OngProject.DataAccess.Seeds
+{
+    public static class OrganizationSeed
+    {
+        public static Organization[] GetData()
+        {
+            var Org = new[]
+            {
+                new Organization()
+                {
+                    Id = 1,
+                    Name = "ONG Somos Más",
+                    Img = "",
+                    Adress = "",
+                    PhoneNumber = 0,
+                    Email = "",
+                    FacebookUrl = "",
+                    InstagramUrl = "",
+                    LinkedInUrl = "",                    
+                    WelcomeText = "",
+                    AboutUsText = "",
+                    IsDeleted = false,
+                    LastEditedAt = DateTime.Now,
+                    CreatedAt = DateTime.Now
+                }
+            };
+
+            return Org;
+        }
+    }
+}
