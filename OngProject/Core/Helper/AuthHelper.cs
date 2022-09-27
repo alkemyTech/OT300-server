5c36287e3cc593ce2810e6173cea17dbdb77572a
DnTo <danto@hotmail.com>
2022-09-26 19:33:46 -0500

    Fixes to generate migration and generate JWT Token

---
+++ OngProject/Core/Helper/AuthHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace OngProject.Core.Helper
+{
+    public static class AuthHelper
+    {
+        public static  string EncryptPassword(string password)
+        {
+            string salt = "OTSampleSalt300";
+            password += salt;
+            byte[] encoded = Encoding.UTF8.GetBytes(password);
+            string encrypted = Convert.ToBase64String(encoded);
+            return encrypted;
+        }
+    }
+}
