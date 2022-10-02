2a305475a0aad2e43a3d2cb3adf7c53127840827
Gustavo Silva <gustavoasc2004@gmail.com>
2022-10-02 14:17:43 -0400

    Send email when a contact is added

---
+++ OngProject/Core/Helper/EmailHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace OngProject.Core.Helper
+{
+    public class EmailHelper
+    {
+        public static string ConvertTemplateToString(string title, string newContent)
+        {
+            string templateFile = Path.Combine(Directory.GetCurrentDirectory(), @"Templates\email_template.html");
+            
+            using (StreamReader sr = File.OpenText(templateFile))
+            {
+                string reader = sr.ReadToEnd();
+                reader = reader.Replace("T&iacute;tulo", title);
+                reader = reader.Replace("Texto del email", newContent);
+
+                return reader;
+            }
+        }
+    }
+}
