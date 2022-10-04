3e3e2a080a37f3a5c23d45bb5f32400c0d167ed2
matute1910 <matute1910@gmail.com>
2022-10-04 13:47:58 -0300

    Endpoint Creación Actividades

--- OngProject/Core/Business/ActivityBusiness.cs
+++ OngProject/Core/Business/ActivityBusiness.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
 using OngProject.Core.Interfaces;
+using OngProject.Core.Models.DTOs;
 using OngProject.Entities;
 using OngProject.Repositories.Interfaces;
 
@@ -11,15 +12,39 @@ namespace OngProject.Core.Business
     public class ActivityBusiness : IActivityBusiness
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageStorageHerlper _imageStorageHerlper;
 
-        public ActivityBusiness(IUnitOfWork unitOfWork)
+        public ActivityBusiness(IUnitOfWork unitOfWork, IImageStorageHerlper imageStorageHerlper)
         {
             _unitOfWork = unitOfWork;
+            _imageStorageHerlper = imageStorageHerlper;
         }
 
-        public Task Add(Activity activity)
+        public async Task<ActivityDTO> Add(ActivityDTO activityDTO)
         {
-            throw new NotImplementedException();
+            var fileName = "Activity-" + activityDTO.Name + ".jpg";
+
+            Activity activities = new Activity()
+            {
+                Name = activityDTO.Name,
+                Content = activityDTO.Content,
+
+            };
+
+            if (activityDTO.ImageFile.Length == 0 || activityDTO.ImageFile is null)
+            {
+                activities.Image = "";
+            }
+            else
+            {
+                activities.Image = await _imageStorageHerlper.UploadImageAsync(activityDTO.ImageFile, fileName);
+
+            }
+
+            await _unitOfWork.ActivityRepository.Add(activities);
+            await _unitOfWork.SaveChangesAsync();
+
+            return activityDTO;
         }
 
         public Task<bool> Delete(int id)
@@ -41,5 +66,7 @@ namespace OngProject.Core.Business
         {
             throw new NotImplementedException();
         }
+
+        
     }
 }
