f97c7906f1249a1a7a8f87de3a6d94f3c2ba8d4e
Gustavo Silva <gustavoasc2004@gmail.com>
2022-10-06 16:20:12 -0400

    Fixed send email method

---
+++ OngProject/Core/Models/AddContactEmail.cs
@@ -0,0 +1,11 @@
+﻿namespace OngProject.Core.Models
+{
+	public class AddContactEmail
+	{
+		public string Subject { get; set; }
+
+		public string Content { get; set; }
+
+		public string Title { get; set; }
+	}
+}
--- OngProject/Core/Models/EmailConfigModel.cs
+++ OngProject/Core/Models/EmailConfigModel.cs
@@ -3,8 +3,10 @@
     public class EmailConfigModel
     {
         public const string WelcomeEmailConfig = "EmailConfigModel";
+        public const string AddContactEmailConfig = "EmailConfigModel";
 
         public WelcomeEmail WelcomeEmail { get; set; }
+        public AddContactEmail AddContactEmail { get; set; }
 
     }
 }
