fd6ac981c9fb362a2efd1f5cbdae331d5d78af17
Geraldson Perez <31606909+Geraldsonp@users.noreply.github.com>
2022-10-13 19:02:04 -0400

    adds swager docs

--- OngProject/Startup.cs
+++ OngProject/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -59,7 +60,11 @@ namespace OngProject
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OngProject", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "OngProject",
+                    Version = "v1",
+                });
             });
             services.ConfigureSwagger();
 
@@ -93,7 +98,11 @@ namespace OngProject
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OngProject v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OngProject v1");
+                    c.RoutePrefix = "api/docs";
+                });
             }
 
 
