60ae322f86d6d8fd50df559d408811a73557f97d
DnTo <danto@hotmail.com>
2022-10-15 00:19:37 -0500

    Public Info Ong Test + temporal fix RequestMethodMiddleware

---
+++ OngProject.Tests/Controllers/OrganizationTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+//using System.Text.Json;
+using OngProject.Core.Mapper;
+using OngProject.Core.Models.DTOs;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngProject.Tests.Controllers
+{
+    [TestClass]
+    public class OrganizationTests
+    {
+        string controller;
+
+        [ClassInitialize]
+        public static async Task ClassInit(TestContext ctx)
+        {
+            // seeds
+            Init.DbContext.Slides.AddRange(DataAccess.Seeds.SlideSeed.GetData());
+            Init.DbContext.Organizations.AddRange(DataAccess.Seeds.OrganizationSeed.GetData());
+            await Init.DbContext.SaveChangesAsync();
+        }
+
+
+        [TestInitialize]
+        public async Task SetupTests()
+        {
+            controller = "organization";
+        }
+
+        //"/api/organization/public
+        [TestMethod]
+        public async Task Public_Should_Return_PublicInfo_From_Org()
+        {
+            //Arrange
+
+
+            //Act
+            var response = await Init.Client.GetAsync($"/api/{controller}/public");
+            var actualJson = await response.Content.ReadAsStringAsync();
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(actualJson));
+
+            var actualObj =
+               JsonSerializer.Create().Deserialize<PublicResponse>(reader);
+
+            //Expected
+            var orgEntity = await Init.DbContext.Organizations.FirstAsync();
+            var slidedEntity = await Init.DbContext.Slides.Where( x => x.OrganizationId == orgEntity.Id).OrderBy( o => o.Order).ToListAsync();
+
+            var expectedOrg = orgEntity.ToPublicDTO();
+            var expectedSlides = slidedEntity.Select( x => x.ToPublicDTO()).ToList();
+
+            PublicResponse expectedObj = new() { orgPubInfoDTO = expectedOrg, slidesOrganizations = expectedSlides };
+
+
+            //StringBuilder sb = new StringBuilder();
+            //StringWriter sw = new StringWriter(sb);
+
+            //JsonWriter writer = new JsonTextWriter(sw);
+
+            var expectedJson = JsonConvert.SerializeObject(expectedObj);
+
+            //Assert
+            Assert.AreEqual(expectedJson.ToLower(), actualJson.ToLower());
+        }
+
+
+        public async Task Public_UpdatePublicData_as_Visitor_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenUser);
+        }
+
+        public async Task Public_UpdatePublicData_as_User_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenUser);
+        }
+
+        public async Task Public_UpdatePublicData_as_Admin_Success()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+        }
+    }
+
+    class PublicResponse
+    {
+        public OrganizationPublicDTO orgPubInfoDTO { get; set; }
+        public List<SlidePublicDTO> slidesOrganizations { get; set; }
+    }
+
+
+
+}
---
+++ OngProject.Tests/Init.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using OngProject.DataAccess;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngProject.Tests
+{
+    [TestClass]
+    public class Init
+    {
+        static TestServer _testServer;
+        public static HttpClient Client;
+        public static OngDbContext DbContext;
+        public static string TokenAdmin;
+        public static string TokenUser;
+
+        [AssemblyInitialize]
+        public static  async Task AssemblyInit(TestContext ctx)
+        {
+            var builder = new WebHostBuilder()
+           .ConfigureAppConfiguration(configurationBuilder =>
+           {
+               configurationBuilder.AddJsonFile("appsettings.development.json");
+           })
+           .UseStartup<Startup>()
+           .ConfigureTestServices(services =>
+           {
+               services.RemoveAll(typeof(DbContextOptions<OngDbContext>));
+               services.AddDbContext<OngDbContext>(opt =>
+               {
+                   opt.UseInMemoryDatabase("ONGTest");
+               });
+           });
+
+            _testServer = new TestServer(builder);
+            Client = _testServer.CreateClient();
+
+            DbContext = _testServer.Services.GetService<OngDbContext>();
+            DbContext.Roles.AddRange(DataAccess.Seeds.RoleSeed.GetData());
+            DbContext.Users.AddRange(DataAccess.Seeds.UserSeed.GetData());
+            DbContext.SaveChanges();
+
+            //Tokens
+            var controller = "auth";
+            //var logins
+            var credentialsAdmin = new { email = "MartaJuarez@gmail.com", password = "123456" };
+            var usersAdmin = new { email = "CarlosJuarez@gmail.com", password = "123456" };
+
+            var jsonAdmin = JsonConvert.SerializeObject(credentialsAdmin);
+            var jsonUser = JsonConvert.SerializeObject(usersAdmin);
+
+            //as admin
+            var response =
+                await Client
+                .PostAsync($"api/{controller}/login",
+                new StringContent(jsonAdmin, Encoding.UTF8, "application/json")
+                );
+
+            TokenAdmin = await response.Content.ReadAsStringAsync();
+            //as user
+            response = await Client
+                .PostAsync($"api/{controller}/login",
+                new StringContent(jsonUser, Encoding.UTF8, "application/json"));
+
+            TokenUser = await response.Content.ReadAsStringAsync();
+        }
+
+        [AssemblyCleanup]
+        public static void Clean()
+        {
+            _testServer.Dispose();
+            Client.Dispose();
+            DbContext.Dispose();
+
+        }
+    }
+}
