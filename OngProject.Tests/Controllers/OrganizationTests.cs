00dee92c11b0123dda28558ff4dd2d59b38110af
DnTo <danto@hotmail.com>
2022-10-16 03:41:10 -0500

    [US-108] Testing Public Info Ong Test + temporal fix RequestMethodMiddleware

--- OngProject.Tests/Controllers/OrganizationTests.cs
+++ OngProject.Tests/Controllers/OrganizationTests.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 //using System.Text.Json;
 using OngProject.Core.Mapper;
 using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,63 +36,235 @@ namespace OngProject.Tests.Controllers
 
 
         [TestInitialize]
-        public async Task SetupTests()
+        public void SetupTests()
         {
             controller = "organization";
         }
 
-        //"/api/organization/public
+
+        /// <summary>
+        /// US-30
+        /// US-69
+        /// US-73
+        /// </summary>
+        /// <returns></returns>
+        //GET"/api/organization/public
         [TestMethod]
-        public async Task Public_Should_Return_PublicInfo_From_Org()
+        public async Task Public_ShouldReturnSpecific_Fields()
         {
             //Arrange
-
+            var properties = new[] { "name", "img", "phonenumber", "adress", "facebookurl", "linkedinurl", "instagramurl" };
 
             //Act
             var response = await Init.Client.GetAsync($"/api/{controller}/public");
             var actualJson = await response.Content.ReadAsStringAsync();
 
-            JsonTextReader reader = new JsonTextReader(new StringReader(actualJson));
+            var expConverter = new ExpandoObjectConverter();
+            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(actualJson, expConverter);
+            dynamic ong = obj.orgPubInfoDTO;
 
-            var actualObj =
-               JsonSerializer.Create().Deserialize<PublicResponse>(reader);
+            //Assert
+            Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+
+            foreach (dynamic item in ong)
+            {
+                string key = item.Key;
+                if (!properties.Contains(key.ToLower())) Assert.Fail(key);
+            }
+        }
+
+        /// <summary>
+        /// US-69
+        /// </summary>
+        /// <returns></returns>
+        //GET"/api/organization/public
+        [TestMethod]
+        public async Task Public_Slides_Should_come_in_order()
+        {
+            //Arrange
+            var response = await Init.Client.GetAsync($"/api/{controller}/public");
+            var actualJson = await response.Content.ReadAsStringAsync();
+
+            var expConverter = new ExpandoObjectConverter();
+            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(actualJson, expConverter);
+
+            //Act
+            dynamic slides = obj.slidesOrganizations;
+
+            //Assert
+            Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+            int order = 0;
+            foreach (dynamic item in slides)
+            {
+                if (item.order >= order) order = (int)item.order;
+                else Assert.Fail($"slides order fail at [{item.text}]");
+            }
+        }
+
+        /// <summary>
+        /// US-69
+        /// </summary>
+        [TestMethod]
+        public async Task Public_Slides_Are_From_Org()
+        {
+            //Arrange
+            //make  it fail
+            //  await Init.DbContext.Slides.AddAsync(new Entities.Slide() { Id = 50, Text = "failing Slide", Order = 20, OrganizationId = 20 });
+            //await Init.DbContext.Slides.AddAsync(new Entities.Slide() { Id = 51, Text = "failing Slide", Order = 20 });
+            //await Init.DbContext.SaveChangesAsync();
 
             //Expected
             var orgEntity = await Init.DbContext.Organizations.FirstAsync();
-            var slidedEntity = await Init.DbContext.Slides.Where( x => x.OrganizationId == orgEntity.Id).OrderBy( o => o.Order).ToListAsync();
 
-            var expectedOrg = orgEntity.ToPublicDTO();
-            var expectedSlides = slidedEntity.Select( x => x.ToPublicDTO()).ToList();
+            //Act
+            //Actual
+            var response = await Init.Client.GetAsync($"/api/{controller}/public");
+            var actualJson = await response.Content.ReadAsStringAsync();
 
-            PublicResponse expectedObj = new() { orgPubInfoDTO = expectedOrg, slidesOrganizations = expectedSlides };
+            JsonTextReader reader = new JsonTextReader(new StringReader(actualJson));
+            PublicResponse actualObj = JsonSerializer.Create().Deserialize<PublicResponse>(reader);
 
-
-            //StringBuilder sb = new StringBuilder();
-            //StringWriter sw = new StringWriter(sb);
-
-            //JsonWriter writer = new JsonTextWriter(sw);
-
-            var expectedJson = JsonConvert.SerializeObject(expectedObj);
+            //Slides are from org
+            var slideNotFromOrg = actualObj.slidesOrganizations.FirstOrDefault(s => s.OrganizationId != orgEntity.Id);
 
             //Assert
-            Assert.AreEqual(expectedJson.ToLower(), actualJson.ToLower());
+            Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+            Assert.IsTrue(slideNotFromOrg == null, $"not all slides are from organization");
         }
 
-
+        /// <summary>
+        /// US-45
+        /// </summary>
+        // GET
+        [TestMethod]
         public async Task Public_UpdatePublicData_as_Visitor_Fail()
         {
-            //  new AuthenticationHeaderValue("Bearer", Init.TokenUser);
+            //Arrange
+            var formContent =
+                new FormUrlEncodedContent(
+                    new[] 
+                    {
+                        new KeyValuePair<string, string>("name", ""),
+                        new KeyValuePair<string, string>("email", "")
+                     });
+
+            //Act
+            var response = await Init.Client.PostAsync($"/api/{controller}/public", formContent);
+            //var actualJson = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(response.StatusCode);
+
+            //Assert
+            Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.Unauthorized);
         }
 
+        /// <summary>
+        /// US-45
+        /// </summary>
+        /// 
+        [TestMethod]
         public async Task Public_UpdatePublicData_as_User_Fail()
         {
-            //  new AuthenticationHeaderValue("Bearer", Init.TokenUser);
+            //Arrange
+            var token =   new AuthenticationHeaderValue("Bearer", Init.TokenUser);
+            Init.Client.DefaultRequestHeaders.Authorization = token;
+
+            var formContent =
+                new FormUrlEncodedContent(
+                    new[]
+                    {
+                        new KeyValuePair<string, string>("name", ""),
+                        new KeyValuePair<string, string>("email", "")
+                     });
+
+            //Act
+            var response = await Init.Client.PostAsync($"/api/{controller}/public", formContent);
+            //var actualJson = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(response.StatusCode);
+
+            //Assert
+            Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.Forbidden);
         }
 
+        /// <summary>
+        /// US-45
+        /// </summary>
+        /// 
+        [TestMethod]
         public async Task Public_UpdatePublicData_as_Admin_Success()
         {
             //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
 
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_Name_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_Image_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_Email_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_WelcomeText_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_Facebook_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_Instagram_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestMethod]
+        public async Task Public_UpdatePublicData_as_Admin_Missing_Linkedin_Fail()
+        {
+            //  new AuthenticationHeaderValue("Bearer", Init.TokenAdmin);
+
+            Assert.Fail("not implemented");
+
+        }
+
+        [TestCleanup]
+        public void CleanTest()
+        {
+            Init.Client.DefaultRequestHeaders.Authorization = null;
         }
     }
 
